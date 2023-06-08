7422aef552afeccb52f4659e3b5e1e7a80f19888
@bs_igor <igorbitencourtsilveira@gmail.com>
2023-06-08 18:19:35 -0300

    Format document

--- src/components/ParametersForm/ParametersForm.tsx
+++ src/components/ParametersForm/ParametersForm.tsx
@@ -1,106 +1,106 @@
-import { List,ListSubheader,ListItem, MenuItem } from "@mui/material";
+import { List, ListSubheader, ListItem, MenuItem } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button'; 
-import { useAppSelector,useAppDispatch } from "../../redux/hooks";
-import {setBoardWidth,setBoardHeight,setDisplacement,setPhaseNumber,setAxisFirstCut} from '../../redux/slices/cutterslice';
+import Button from '@mui/material/Button';
+import { useAppSelector, useAppDispatch } from "../../redux/hooks";
+import { setBoardWidth, setBoardHeight, setDisplacement, setPhaseNumber, setAxisFirstCut } from '../../redux/slices/cutterslice';
 import { eAxis, eAxisStrings } from "../../common/cutter/eAxis";
 import { useMemo } from "react";
 import ParametersFormProps from "./ParametersFormProps";
 
 const subheader = (<ListSubheader>Parameters</ListSubheader>);
 
-export default function ParametersForm(props: ParametersFormProps){
+export default function ParametersForm(props: ParametersFormProps) {
 
-    const {boardWidth,boardHeight,displacement,phaseNumber,axisFirstCut}  = useAppSelector( state => state.cutter);
+    const { boardWidth, boardHeight, displacement, phaseNumber, axisFirstCut } = useAppSelector(state => state.cutter);
     const dispatcher = useAppDispatch();
-    const performNewCutAllowed = useMemo( () => {
-        return displacement === 0 || 
-               displacement === null || 
-               displacement === undefined ||
-               isNaN(displacement)
-    },[displacement]);
-    return(
-        <> 
+    const performNewCutAllowed = useMemo(() => {
+        return displacement === 0 ||
+            displacement === null ||
+            displacement === undefined ||
+            isNaN(displacement)
+    }, [displacement]);
+    return (
+        <>
             <List subheader={subheader}>
                 <ListItem>
-                    <TextField 
-                        sx={{height:'40px', width:'150px'}}
+                    <TextField
+                        sx={{ height: '40px', width: '150px' }}
                         label="Axis first cut"
                         variant="outlined"
                         select
                         size="small"
-                        value={axisFirstCut} 
-                        onChange={(e) => { 
-                            const value = eAxis[e.target.value as eAxisStrings]; 
+                        value={axisFirstCut}
+                        onChange={(e) => {
+                            const value = eAxis[e.target.value as eAxisStrings];
                             dispatcher(setAxisFirstCut(value))
                         }}
                     >
                         {Object.keys(eAxis).map((option) => (
                             <MenuItem key={option} value={option}>
-                            {option}
+                                {option}
                             </MenuItem>
                         ))}
-                         
+
                     </TextField>
                 </ListItem>
                 <ListItem>
-                    <TextField 
-                        sx={{height:'40px'}}
+                    <TextField
+                        sx={{ height: '40px' }}
                         label="Board width"
                         variant="outlined"
                         size="small"
                         defaultValue={boardWidth}
                         value={boardWidth}
-                        onChange={(e) => dispatcher(setBoardWidth(parseInt( e.target.value )))}
-                     />
+                        onChange={(e) => dispatcher(setBoardWidth(parseInt(e.target.value)))}
+                    />
                 </ListItem>
-                <ListItem>    
-                    <TextField 
-                        sx={{height:'40px'}}
+                <ListItem>
+                    <TextField
+                        sx={{ height: '40px' }}
                         label="Board height"
                         variant="outlined"
                         size="small"
                         defaultValue={boardHeight}
                         value={boardHeight}
-                        onChange={(e) => dispatcher(setBoardHeight(parseInt( e.target.value )))}
+                        onChange={(e) => dispatcher(setBoardHeight(parseInt(e.target.value)))}
                     />
                 </ListItem>
-                <ListItem>    
-                    <TextField 
-                        sx={{height:'40px'}}
+                <ListItem>
+                    <TextField
+                        sx={{ height: '40px' }}
                         label="Displacement"
                         variant="outlined"
                         size="small"
                         defaultValue={displacement}
                         value={displacement}
-                        onChange={(e) => dispatcher(setDisplacement(parseInt( e.target.value )))}
+                        onChange={(e) => dispatcher(setDisplacement(parseInt(e.target.value)))}
                     />
                 </ListItem>
-                <ListItem>    
-                    <TextField 
-                        sx={{height:'40px'}}
+                <ListItem>
+                    <TextField
+                        sx={{ height: '40px' }}
                         label="Phase number"
                         variant="outlined"
                         size="small"
                         defaultValue={phaseNumber}
                         value={phaseNumber}
-                        onChange={(e) => dispatcher(setPhaseNumber(parseInt( e.target.value )))}
+                        onChange={(e) => dispatcher(setPhaseNumber(parseInt(e.target.value)))}
                     />
                 </ListItem>
                 <ListItem>
-                <Stack spacing={2} direction="row">
-                    <Button 
-                        variant="contained" 
-                        onClick={props.performNewCut}
-                        disabled = { performNewCutAllowed}>Cut!</Button>
-                    <Button 
-                        variant="contained"
-                        onClick={props.clearCurrentBoard}
+                    <Stack spacing={2} direction="row">
+                        <Button
+                            variant="contained"
+                            onClick={props.performNewCut}
+                            disabled={performNewCutAllowed}>Cut!</Button>
+                        <Button
+                            variant="contained"
+                            onClick={props.clearCurrentBoard}
                         >Clear board</Button>
                     </Stack>
-                </ListItem>            
-            </List> 
+                </ListItem>
+            </List>
         </>
     );
 }
