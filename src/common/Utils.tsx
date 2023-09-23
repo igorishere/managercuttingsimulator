9b939f858ea25f324b7a65b461729b17465faf43
@bs_igor <igorbitencourtsilveira@gmail.com>
2023-09-23 16:21:39 -0300

    Refactor: Make inch to millimeters a constant

--- src/common/Utils.tsx
+++ src/common/Utils.tsx
@@ -1,11 +1,12 @@
 const DEFAULT_SCREEN_DPI = 96;
+const INCH_CONVERTED_TO_MILLIMETERS = 25.4;
 
 export class Utils {
     static ConvertMilimetersToPixels(measureInMilimeters: number): number {
-        return measureInMilimeters * (DEFAULT_SCREEN_DPI / 25.4);
+        return measureInMilimeters * (DEFAULT_SCREEN_DPI / INCH_CONVERTED_TO_MILLIMETERS);
     }
 
     static ConvertPixelsToMilimeters(measureInPixels: number): number {
-        return measureInPixels / (DEFAULT_SCREEN_DPI / 25.4);
+        return measureInPixels / (DEFAULT_SCREEN_DPI / INCH_CONVERTED_TO_MILLIMETERS);
     }
 }
